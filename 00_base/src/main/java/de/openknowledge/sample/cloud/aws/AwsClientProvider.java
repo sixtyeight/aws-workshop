ba397b52f4b00ca58abb5074ba99b62d78f005d0
sixtyeight <christian_sl@gmx.at>
2017-11-04 17:56:31 +0100

    dummy awsaccount settings and local endpoints

--- 00_base/src/main/java/de/openknowledge/sample/cloud/aws/AwsClientProvider.java
+++ 00_base/src/main/java/de/openknowledge/sample/cloud/aws/AwsClientProvider.java
@@ -1,5 +1,8 @@
 package de.openknowledge.sample.cloud.aws;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDBAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDBClient;
@@ -8,40 +11,52 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 public class AwsClientProvider {
 
-        private static Region DEFAULT_REGION = Region.EU_CENTRAL_1;
+	private static Region DEFAULT_REGION = Region.EU_CENTRAL_1;
 
-        // blocking & synchronous clients
+	// docker run -p 4572:4572 -p 4569:4569 atlassianlabs/localstack:0.6.2
 
-        public static S3Client getS3Client() {
-                return S3Client.builder()
-                        .credentialsProvider(AwsAccount.defaultCredentialProvider())
-                        .region(DEFAULT_REGION)
-                        .build();
-        }
+	private static String S3_ENDPOINT = "http://localhost:4572";
 
-        public static DynamoDBClient getDynamoDBClient() {
-                return DynamoDBClient.builder()
-                        .credentialsProvider(AwsAccount.defaultCredentialProvider())
-                        .region(DEFAULT_REGION)
-                        .build();
-        }
+	private static String DYNAMODB_ENDPOINT = "http://localhost:4569";
 
+	// blocking & synchronous clients
 
-        // non blocking & asynchronous clients
+	public static S3Client getS3Client() {
+		try {
+			return S3Client.builder().credentialsProvider(AwsAccount.defaultCredentialProvider()).region(DEFAULT_REGION)
+					.region(DEFAULT_REGION).endpointOverride(new URI(S3_ENDPOINT)).build();
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
-        public static S3AsyncClient getS3AsyncClient() {
-                return S3AsyncClient.builder()
-                        .credentialsProvider(AwsAccount.defaultCredentialProvider())
-                        .region(DEFAULT_REGION)
-                        .build();
-        }
+	public static DynamoDBClient getDynamoDBClient() {
+		try {
+			return DynamoDBClient.builder().credentialsProvider(AwsAccount.defaultCredentialProvider())
+					.region(DEFAULT_REGION).endpointOverride(new URI(DYNAMODB_ENDPOINT)).build();
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
-        public static DynamoDBAsyncClient getDynamoDBAsyncClient() {
-                return DynamoDBAsyncClient.builder()
-                        .credentialsProvider(AwsAccount.defaultCredentialProvider())
-                        .region(DEFAULT_REGION)
-                        .build();
-        }
+	// non blocking & asynchronous clients
 
+	public static S3AsyncClient getS3AsyncClient() {
+		try {
+			return S3AsyncClient.builder().credentialsProvider(AwsAccount.defaultCredentialProvider())
+					.region(DEFAULT_REGION).endpointOverride(new URI(S3_ENDPOINT)).build();
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	public static DynamoDBAsyncClient getDynamoDBAsyncClient() {
+		try {
+			return DynamoDBAsyncClient.builder().credentialsProvider(AwsAccount.defaultCredentialProvider())
+					.region(DEFAULT_REGION).endpointOverride(new URI(DYNAMODB_ENDPOINT)).build();
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
 }
